8cf55de67249e04bfcf229fe8c7cd33450f6b4a3
bilux <i.bilux@gmail.com>
2020-04-26 00:36:22 +0100

    Revert "ANDROID: cpufreq: stats: Fix sleeping while atomic in cpufreq_task_stats_init"
    This reverts commit 31d210e6f84a50a97e99711c5de1c4f97407088c.

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -510,7 +510,7 @@ void cpufreq_task_stats_alloc(struct task_struct *p)
 	 * cpus
 	 */
 	alloc_size = p->max_states * sizeof(p->time_in_state[0]);
-	temp = kzalloc(alloc_size, GFP_ATOMIC);
+	temp = kzalloc(alloc_size, GFP_KERNEL);
 
 	spin_lock_irqsave(&task_time_in_state_lock, flags);
 	p->time_in_state = temp;
