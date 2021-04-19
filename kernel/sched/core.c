da55e26df621fe568ed5cd306640c0b4f6441c6f
bilux <i.bilux@gmail.com>
2021-04-19 02:20:08 +0100

    Revert "ANDROID: cpufreq_stat: add per task/uid/freq stats"
    This reverts commit 689a43e091e56a897ee261f83d3b1fa0e94e350b.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -73,7 +73,6 @@
 #include <linux/init_task.h>
 #include <linux/binfmts.h>
 #include <linux/context_tracking.h>
-#include <linux/cpufreq.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -1822,10 +1821,6 @@ static void __sched_fork(struct task_struct *p)
 #endif
 #ifdef CONFIG_SCHEDSTATS
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
-#endif
-
-#ifdef CONFIG_CPU_FREQ_STAT
-	cpufreq_task_stats_init(p);
 #endif
 
 	INIT_LIST_HEAD(&p->rt.run_list);
